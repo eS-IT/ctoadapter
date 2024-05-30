e9b366fb3a3f2d7cf36c9dda6b077eb779aaf2a0
Patrick Froch <patrick.froch@easySolutionsIT.de>
2024-05-30 12:49:24 +0200

    Added: Dbafs

---
+++ Classes/Services/Adapter/Dbafs.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @package     ctoadapter
+ * @since       30.05.2024 - 12:47
+ * @author      Patrick Froch <info@easySolutionsIT.de>
+ * @see         http://easySolutionsIT.de
+ * @copyright   e@sy Solutions IT 2024
+ * @license     EULA
+ */
+
+declare(strict_types=1);
+
+namespace Esit\Ctoadapter\Classes\Services\Adapter;
+
+class Dbafs extends Adapter
+{
+}
